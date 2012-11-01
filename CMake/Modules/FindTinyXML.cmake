7e28e806a95547c36462dfa024f3ed88d21a01a9
vobject <vobject@gmail.com>
2012-11-01 18:34:16 +0100

    fix compilation with tinyxml for mingw

--- CMake/Modules/FindTinyXML.cmake
+++ CMake/Modules/FindTinyXML.cmake
@@ -1,28 +1,17 @@
-# File copied from http://code.google.com/p/mgep/source/browse/branches/clientv2/CMakeModules/FindTinyXML.cmake?r=513
-
 # - Find TinyXML
-# Find the native TinyXML includes and library
+# Find the TinyXML include directory and library
 #
 #   TINYXML_FOUND       - True if TinyXML found.
-#   TINYXML_INCLUDE_DIR - where to find tinyxml.h, etc.
-#   TINYXML_LIBRARIES   - List of libraries when using TinyXML.
+#   TINYXML_INCLUDE_DIR - Where to find tinyxml.h
+#   TINYXML_LIBRARIES   - List of libraries for linking.
 #
 
-IF( TINYXML_INCLUDE_DIR )
-    # Already in cache, be silent
-    SET( TinyXML_FIND_QUIETLY TRUE )
-ENDIF( TINYXML_INCLUDE_DIR )
+FIND_PATH(TINYXML_INCLUDE_DIR tinyxml.h)
+FIND_LIBRARY(TINYXML_LIBRARIES tinyxml)
 
-FIND_PATH( TINYXML_INCLUDE_DIR "tinyxml.h"
-           PATH_SUFFIXES "tinyxml" )
-
-FIND_LIBRARY( TINYXML_LIBRARIES
-              NAMES "tinyxml"
-              PATH_SUFFIXES "tinyxml" )
-
-# handle the QUIETLY and REQUIRED arguments and set TINYXML_FOUND to TRUE if
-# all listed variables are TRUE
-INCLUDE( "FindPackageHandleStandardArgs" )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( "TinyXML" DEFAULT_MSG TINYXML_INCLUDE_DIR TINYXML_LIBRARIES )
-
-MARK_AS_ADVANCED( TINYXML_INCLUDE_DIR TINYXML_LIBRARIES )
+IF(TINYXML_INCLUDE_DIR AND TINYXML_LIBRARIES)
+	MESSAGE(STATUS "TinyXML library found: ${TINYXML_LIBRARIES}")
+	SET(TINYXML_FOUND 1)
+ELSE(TINYXML_INCLUDE_DIR AND TINYXML_LIBRARIES)
+	MESSAGE(STATUS "Unable to find TinyXML")
+ENDIF(TINYXML_INCLUDE_DIR AND TINYXML_LIBRARIES)
